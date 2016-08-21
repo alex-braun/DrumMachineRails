364fb81f0b2b7dec8eb00d390ccdaa5782090c3f
alex-braun <braunacb@gmail.com>
2016-08-20 23:34:04 -0400

    Added resources users do beats.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
+
+  resources :users do
+    resources :beats
+  end
 end
