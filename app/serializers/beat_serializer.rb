15e2eaf8a55465c6fae58171a08e7e900a050a47
alex-braun <braunacb@gmail.com>
2016-08-20 23:32:29 -0400

    Serializer changed to has_one user.

--- app/serializers/beat_serializer.rb
+++ app/serializers/beat_serializer.rb
@@ -1,4 +1,4 @@
 class BeatSerializer < ActiveModel::Serializer
-  attributes :id, :name, :kick, :snare, :hatClose, :hatOpen, :clap
+  attributes :id, :name, :kick, :snare, :hatClose, :hatOpen, :clap, :user_id
   has_one :user
 end
